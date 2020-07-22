64f8d1fe312238f14316e719aed9cae05368924e
hritikpadigela <hritikpadigela98@gmail.com>
2020-07-23 02:34:59 +0530

    Initial Commit

---
+++ epam_task_5_Hritik/epamtask5/src/main/java/com/hritik/epamtask5/MainClass.java
@@ -0,0 +1,12 @@
+package com.hritik.epamtask5;
+
+import com.hritik.epamtask5.*;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Interest.InterestCalculation();
+		Construction.Cost();
+	}
+
+}
